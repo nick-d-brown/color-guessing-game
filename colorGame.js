72b4602594df44071ea6cea453c719c17e5648ed
Nick Brown <n.brown.professional@gmail.com>
2018-04-03 22:20:27 -0600

    Adds HTML, CSS, JS/ also basic functionality

---
+++ colorGame.js
@@ -0,0 +1,34 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+
+for (var i =0; i < squares.length; i++) {
+    // Add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+
+    //Add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        // Grab color of picked square
+    clickedColor = this.style.backgroundColor;
+    
+        // compare color to pickedColor
+    if (clickedColor === pickedColor) {
+        alert("correct");
+    } else {
+        alert("wrong");
+    }
+
+    });
+}
